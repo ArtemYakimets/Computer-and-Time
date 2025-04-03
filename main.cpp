4a1e1209f959362d627120c7321fbe635d0b54c9
ArtemYakimets <artemyakimets0110@gmail.com>
2025-04-04 00:33:46 +0300

    Add syscall time experiment

--- main.cpp
+++ main.cpp
@@ -5,147 +5,107 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <numeric>
 
-// Measure system time (CLOCK_REALTIME analog)
-void measureSystemTime() {
+using namespace std;
+
+struct TimerMetrics {
+    double resolution;
+    double resolution_error;
+    double a1; // Syscall entry time 
+    double a2; // Syscall exit time
+};
+
+// Measure timer latency for the timer
+template<typename TimerFunc>
+TimerMetrics measureTimerLatency(TimerFunc timer, const string& name) {
+    const int N = 10000;
+    vector<double> total_times(N);
+    vector<double> t1(N), t2(N);
+
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+
+    // Measure general time of consistent calls
+    for (int i = 0; i < N; ++i) {
+        LARGE_INTEGER start, end;
+        QueryPerformanceCounter(&start);
+        timer(); // First call
+        timer(); // Second call
+        QueryPerformanceCounter(&end);
+        
+        total_times[i] = (end.QuadPart - start.QuadPart) * 1e6 / freq.QuadPart; // mks
+    }
+
+    // Measure statistics
+    double avg_total = accumulate(total_times.begin(), total_times.end(), 0.0) / N;
+    double a_avg = avg_total / 2; // A1 + A2
+
+    // A1 ~= A2
+    TimerMetrics metrics;
+    metrics.a1 = a_avg / 2;
+    metrics.a2 = a_avg / 2;
+
+    // Measure timer resolution
+    vector<double> resolutions(N);
+    for (int i = 0; i < N; ++i) {
+        LARGE_INTEGER start, end;
+        timer();
+        QueryPerformanceCounter(&start);
+        timer();
+        QueryPerformanceCounter(&end);
+        resolutions[i] = (end.QuadPart - start.QuadPart) * 1e6 / freq.QuadPart;
+    }
+
+    double res_mean = accumulate(resolutions.begin(), resolutions.end(), 0.0) / N;
+    double sq_sum = inner_product(resolutions.begin(), resolutions.end(), resolutions.begin(), 0.0);
+    metrics.resolution = res_mean;
+    metrics.resolution_error = sqrt((sq_sum - N * res_mean * res_mean) / (N - 1));
+
+    return metrics;
+}
+
+// Wrapper functions of each timer
+void callSystemTime() {
     FILETIME ft;
     GetSystemTimePreciseAsFileTime(&ft);
-    ULARGE_INTEGER uli;
-    uli.LowPart = ft.dwLowDateTime;
-    uli.HighPart = ft.dwHighDateTime;
-    std::cout << "System time (100ns): " << uli.QuadPart << std::endl;
 }
 
-// Measure CLOCK_REALTIME analog
-void measureRealtimeClock() {
-    FILETIME ft1, ft2;
-    const int N = 1000; 
-    double sum = 0.0;
-    double sum_sq = 0.0;
-
-    for (int i = 0; i < N; ++i) {
-        GetSystemTimePreciseAsFileTime(&ft1);
-        GetSystemTimePreciseAsFileTime(&ft2);
-
-        // 100 ns interval
-        ULARGE_INTEGER uli1, uli2;
-        uli1.LowPart = ft1.dwLowDateTime;
-        uli1.HighPart = ft1.dwHighDateTime;
-        uli2.LowPart = ft2.dwLowDateTime;
-        uli2.HighPart = ft2.dwHighDateTime;
-
-        double diff = static_cast<double>(uli2.QuadPart - uli1.QuadPart) * 100.0; // ns
-        sum += diff;
-        sum_sq += diff * diff;
-    }
-
-    double mean = sum / N; 
-    double std_dev = sqrt((sum_sq - sum * sum / N) / (N - 1));
-
-    std::cout << "CLOCK_REALTIME (analog):\n"
-              << "  Average resolution: " << mean << " ns\n"
-              << "  Error rate: +-" << std_dev << " ns\n";
+void callPerformanceCounter() {
+    LARGE_INTEGER pc;
+    QueryPerformanceCounter(&pc);
 }
 
-// Measure Performance Counter (HPET analog) with error calculation
-void measurePerformanceCounter() {
-    LARGE_INTEGER freq;
-    QueryPerformanceFrequency(&freq);
-    
-    const int N = 1000;
-    std::vector<double> samples(N);
-    double sum = 0.0;
-    double sum_sq = 0.0;
-
-    for (int i = 0; i < N; ++i) {
-        LARGE_INTEGER start, end;
-        QueryPerformanceCounter(&start);
-        QueryPerformanceCounter(&end);
-        
-        double diff = (end.QuadPart - start.QuadPart) * 1e6 / freq.QuadPart; // microseconds
-        samples[i] = diff;
-        sum += diff;
-        sum_sq += diff * diff;
-    }
-
-    double mean = sum / N;
-    double std_dev = sqrt((sum_sq - sum * sum / N) / (N - 1));
-
-    std::cout << "Performance Counter:\n"
-              << "  Average resolution: " << mean << " mks\n"
-              << "  Resolution error: +-" << std_dev << " mks\n"
-              << "  Min: " << *min_element(samples.begin(), samples.end()) << " mks\n"
-              << "  Max: " << *max_element(samples.begin(), samples.end()) << " mks\n";
-}
-
-// Measure RDTSC resolution with error calculation
-void measureRDTSC() {
-    const int N = 1000;
-    std::vector<unsigned long long> samples(N);
-    double sum = 0.0;
-    double sum_sq = 0.0;
-
-    for (int i = 0; i < N; ++i) {
-        unsigned long long start = __rdtsc();
-        unsigned long long end = __rdtsc();
-        
-        unsigned long long diff = end - start;
-        samples[i] = diff;
-        sum += diff;
-        sum_sq += static_cast<double>(diff) * diff;
-    }
-
-    double mean = sum / N;
-    double std_dev = sqrt((sum_sq - sum * sum / N) / (N - 1));
-
-    std::cout << "RDTSC:\n"
-              << "  Average resolution: " << mean << " cycles\n"
-              << "  Resolution error: +-" << std_dev << " cycles\n"
-              << "  Min: " << *min_element(samples.begin(), samples.end()) << " cycles\n"
-              << "  Max: " << *max_element(samples.begin(), samples.end()) << " cycles\n";
-}
-
-// Calibrate RDTSC with error calculation
-void calibrateRDTSC() {
-    const int N = 10;  // Reduced number due to long measurement time
-    std::vector<double> samples(N);
-    double sum = 0.0;
-    double sum_sq = 0.0;
-    
-    LARGE_INTEGER freq;
-    QueryPerformanceFrequency(&freq);
-
-    for (int i = 0; i < N; ++i) {
-        LARGE_INTEGER start, end;
-        QueryPerformanceCounter(&start);
-        unsigned long long tsc_start = __rdtsc();
-        Sleep(100);  // Reduced from 1000ms to 100ms for faster testing
-        QueryPerformanceCounter(&end);
-        unsigned long long tsc_end = __rdtsc();
-        
-        double sec = (end.QuadPart - start.QuadPart) / static_cast<double>(freq.QuadPart);
-        double cycles_per_sec = (tsc_end - tsc_start) / sec;
-        samples[i] = cycles_per_sec;
-        sum += cycles_per_sec;
-        sum_sq += cycles_per_sec * cycles_per_sec;
-    }
-
-    double mean = sum / N;
-    double std_dev = sqrt((sum_sq - sum * sum / N) / (N - 1));
-
-    std::cout << "RDTSC Calibration:\n"
-              << "  Average frequency: " << mean << " Hz (" << mean/1e9 << " GHz)\n"
-              << "  Calibration error: +-" << std_dev << " Hz (+-" << std_dev/1e9 << " GHz)\n"
-              << "  Min: " << *min_element(samples.begin(), samples.end())/1e9 << " GHz\n"
-              << "  Max: " << *max_element(samples.begin(), samples.end())/1e9 << " GHz\n";
+void callRDTSC() {
+    __rdtsc();
 }
 
 int main() {
-    std::cout << "===== Measure time =====" << std::endl;
-    measureSystemTime();
-    measureRealtimeClock();
-    measurePerformanceCounter();
-    measureRDTSC();
-    calibrateRDTSC();
+    // Measure data of all the timers
+    TimerMetrics system_time = measureTimerLatency(callSystemTime, "System Time");
+    TimerMetrics perf_counter = measureTimerLatency(callPerformanceCounter, "Performance Counter");
+    TimerMetrics rdtsc = measureTimerLatency(callRDTSC, "RDTSC");
+
+    // Print the result as table
+    cout << "| Timer Name           | Resolution (μs) | Resolution Error | A1 (mks) | A2 (mks) |\n";
+    cout << "|----------------------|------------------|------------------|---------|---------|\n";
+    printf("| %-20s | %-16.3f | %-16.3f | %-7.3f | %-7.3f |\n", 
+           "System Time", system_time.resolution, system_time.resolution_error, system_time.a1, system_time.a2);
+    printf("| %-20s | %-16.3f | %-16.3f | %-7.3f | %-7.3f |\n", 
+           "Performance Counter", perf_counter.resolution, perf_counter.resolution_error, perf_counter.a1, perf_counter.a2);
+    printf("| %-20s | %-16.3f | %-16.3f | %-7.3f | %-7.3f |\n", 
+           "RDTSC", rdtsc.resolution, rdtsc.resolution_error, rdtsc.a1, rdtsc.a2);
+
+    // Calibrate RDTSC
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    unsigned long long tsc_start = __rdtsc();
+    Sleep(100);
+    unsigned long long tsc_end = __rdtsc();
+    double cycles_per_sec = (tsc_end - tsc_start) / 0.1; // 100ms = 0.1s
+    
+    cout << "\nRDTSC Calibration:\n";
+    cout << "  CPU Frequency: " << cycles_per_sec / 1e9 << " GHz\n";
+
     return 0;
 }
